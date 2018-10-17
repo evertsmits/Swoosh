befb149f05e102ac996d626779309dfb168c5a86
evertsmits <egsdevelopmentmail@gmail.com>
2018-10-17 19:57:37 +0200

    selection and intent extra

---
+++ app/src/main/java/com/egsdevelopment/swoosh/Constants.kt
@@ -0,0 +1,8 @@
+package com.egsdevelopment.swoosh
+
+/**
+ * Created by Evert Smits
+ * Constants file
+ */
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/egsdevelopment/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/egsdevelopment/swoosh/LeagueActivity.kt
@@ -4,16 +4,52 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+    fun onMensClicked(view: View) {
+        resetClicked()
+        mensLeagueBtn.isChecked = true
+        selectedLeague = "mens"
     }
+
+    fun onWomensClicked(view: View) {
+        resetClicked()
+        womensLeagueBtn.isChecked = true
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        resetClicked()
+        coedLeagueBtn.isChecked = true
+        selectedLeague = "coed"
+    }
+
+    fun resetClicked() {
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+        mensLeagueBtn.isChecked = false
+    }
+
+
+    fun leagueNextClicked(view: View) {
+        if(selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
